3303e20f928d227c5cafee7d92a82d8ea6bd1f43
David Frost <107281913+DavidfNZ@users.noreply.github.com>
2022-07-24 16:17:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My test script')
+streamlit.title('View Our Fruit List - Add Your Favorites !')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -50,9 +50,10 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # Add a button to load the fruit            
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
@@ -65,4 +66,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
